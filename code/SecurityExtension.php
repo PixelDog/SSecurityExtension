8ae54760e34c23c6b1d46abd9e68c8e3812f48bc
PixelDog <kaozotto@gmail.com>
2018-04-11 21:53:37 -0700

    minor change

--- code/SecurityExtension.php
+++ code/SecurityExtension.php
@@ -28,15 +28,15 @@ class SecurityExtension extends Extension {
 
 		if ( getenv( 'HTTP_CLIENT_IP' ) )
 			$ip_address = getenv( 'HTTP_CLIENT_IP' );
-		else if( getenv( 'HTTP_X_FORWARDED_FOR' ) )
+		elseif( getenv( 'HTTP_X_FORWARDED_FOR' ) )
 			$ip_address = getenv( 'HTTP_X_FORWARDED_FOR' );
-		else if( getenv( 'HTTP_X_FORWARDED' ) )
+		elseif( getenv( 'HTTP_X_FORWARDED' ) )
 			$ip_address = getenv( 'HTTP_X_FORWARDED' );
-		else if( getenv( 'HTTP_FORWARDED_FOR' ) )
+		elseif( getenv( 'HTTP_FORWARDED_FOR' ) )
 			$ip_address = getenv( 'HTTP_FORWARDED_FOR' );
-		else if( getenv( 'HTTP_FORWARDED' ) )
+		elseif( getenv( 'HTTP_FORWARDED' ) )
 		   $ip_address = getenv( 'HTTP_FORWARDED' );
-		else if(getenv( 'REMOTE_ADDR') )
+		elseif(getenv( 'REMOTE_ADDR') )
 			$ip_address = getenv( 'REMOTE_ADDR' );
 		else
 			$ip_address = 'UNKNOWN';
